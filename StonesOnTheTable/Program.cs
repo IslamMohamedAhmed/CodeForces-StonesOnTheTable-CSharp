97c07e651b1803c198967430a65a817cebfad143
unknown <islammohamedahmed49@gmail.com>
2024-04-14 14:15:52 +0200

    sott

---
+++ StonesOnTheTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+
+int n,counter=0;
+
+
+n = ToInt32(ReadLine());
+string str = ReadLine();
+
+for(int i=0;i<str.Length; i++)
+{
+    if (i != n - 1)
+    {
+        if (str[i] == str[i + 1])
+        {
+            counter++;
+        }
+    }
+}
+
+
+WriteLine(counter);
